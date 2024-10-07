f2e91387c41ad29c40332ef0895a7caa106c9706
Jonathan Carvalho <jonathan_dias_carvalho@outlook.com>
2024-10-07 13:12:08 -0300

    up

---
+++ src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+import { UsersService } from './users.service';
+import { ApiBody } from '@nestjs/swagger';
+import { UserDto } from './dtos/user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  indexedDB(): User[] {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  @ApiBody({ type: UserDto})
+  create(@Body() user: UserDto): User {
+    return this.usersService.create(user);
+  }
+}
